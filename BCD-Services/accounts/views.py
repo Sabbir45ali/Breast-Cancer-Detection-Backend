97890231f59b5d6801f2aa1ed3b8765728f6712c
ronit-here <cyphing.god2524@gmail.com>
2025-02-15 13:25:17 +0530

    deletd unnecessary files

--- BCD-Services/accounts/views.py
+++ BCD-Services/accounts/views.py
@@ -1,14 +1,2 @@
-# Importing the 'redirect' the user to a new url as ,
-# Django -> localhost 8000
-# React -> localhost 3000
-
 from django.shortcuts import render , redirect
 from django.http import JsonResponse
-
-# Redirect the user to the React Frontend Email confirmation page
-def email_confirmation(request, key):
-    return redirect(f"http://localhost:3000/dj-rest-auth/registration/account-confirm-email/{key}")
-
-#Redirects the user to the React frontend password reset confirmation URL
-def reset_password_confirm(request, uid, token):
-    return redirect(f"http://localhost:3000/reset/password/confirm/{uid}/{token}")
--- BCD-Services/backend_site/urls.py
+++ BCD-Services/backend_site/urls.py
@@ -6,15 +6,4 @@ from accounts.views import email_confirmation , reset_password_confirm
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    # Authentication endpoints from dj-rest-auth
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    
-    # It's an Custom endpoint for email confirmation (always write it before dj-rest-registration)
-    path('dj-rest-auth/registration/account-confirm-email/<str:key>', email_confirmation),
-    
-    # Resistration endpoints for dj-rest-auth
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    
-    # It's an Custom endpoint for email confirmation during password reset
-    path('reset/password/confirm/<int:uid>/<str:token>' , reset_password_confirm , name = "password_reset_confirm"),
 ]
