186de667fd85dc45ce730eb99255fa8aa7e317f3
ronit-here <cyphing.god2524@gmail.com>
2025-04-17 22:57:26 +0530

    DONE SOME CHANGES

--- BCD-Services/accounts/views.py
+++ BCD-Services/accounts/views.py
@@ -1,7 +1,15 @@
+from rest_framework import viewsets
+from .models import PersonalDetails
+from .serializers import PersonalDetailsSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import ImageUploadSerializer
+
+class PersonalDetailsViewSet(viewsets.ModelViewSet):
+    queryset = PersonalDetails.objects.all()
+    serializer_class = PersonalDetailsSerializer
+
 
 class ImageUploadView(APIView):
     def post(self, request, format=None):
