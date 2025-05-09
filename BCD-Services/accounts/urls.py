03f79332afc6a0d4279ed3ecbef69dd846cc96de
ronit-here <cyphing.god2524@gmail.com>
2025-05-09 22:15:56 +0530

    modify the model

--- BCD-Services/accounts/urls.py
+++ BCD-Services/accounts/urls.py
@@ -1,13 +1,19 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import ImageUploadView, PersonalDetailsViewSet
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-
-router = DefaultRouter()
-router.register(r'personal-details', PersonalDetailsViewSet)
+from .views import (
+    personal_details_view,
+    personal_detail_view,
+    ImageUploadView
+)
 
 urlpatterns = [
+
+    path('personal-details/', personal_details_view, name='personal-details'),
+    path('personal-details/<str:user_id>/', personal_detail_view, name='personal-detail'),
+
     path('memo-image/', ImageUploadView.as_view(), name='upload-image'),
-    path('', include(router.urls)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
