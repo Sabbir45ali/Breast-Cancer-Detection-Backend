105c65e63fa509a26f9b8f9554177b7a7bea4c62
Sabbir45ali <sabbir84b@gmail.com>
2025-06-23 00:11:45 +0530

    Refactor account URLs to improve routing and add new endpoints for image upload and cancer data submission

--- BCD-Services/accounts/urls.py
+++ BCD-Services/accounts/urls.py
@@ -1,20 +1,26 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ImageUploadView, PersonalDetailsViewSet, submit_cancer_data
 from django.conf import settings
 from django.conf.urls.static import static
 
-router = DefaultRouter()
-router.register(r'personal-details', PersonalDetailsViewSet)
 from .views import (
+    ImageUploadView,
+    personal_detail_view,
     personal_details_view,
     personal_detail_view,
-    ImageUploadView
+    submit_cancer_data
 )
+
+router = DefaultRouter()
+router.register(r'personal-details', personal_detail_view)
+
 urlpatterns = [
+    path('memo-image/', ImageUploadView.as_view(), name='upload-image'),
+    path('cancer-data/', submit_cancer_data, name='submit-cancer-data'),
+    path('', include(router.urls)),
     path('personal-details/', personal_details_view, name='personal-details'),
     path('personal-details/<str:user_id>/', personal_detail_view, name='personal-detail'),
-    path('', include(router.urls)),
-    path('memo-image/', ImageUploadView.as_view(), name='upload-image'),
-]; 
+]
+
+# Serve media files during development
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
