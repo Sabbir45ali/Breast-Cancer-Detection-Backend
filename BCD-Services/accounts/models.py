de0c514b881551200e9a2f3264063ecc64899fb5
Sabbir45ali <sabbir84b@gmail.com>
2025-06-23 00:04:49 +0530

    Update .gitignore, add Cloudinary storage configuration, and enhance media settings

--- BCD-Services/accounts/models.py
+++ BCD-Services/accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import RegexValidator
+from cloudinary_storage.storage import MediaCloudinaryStorage
 
 class PersonalDetails(models.Model):
     User_id = models.CharField(max_length=255, primary_key=True, unique=True)
