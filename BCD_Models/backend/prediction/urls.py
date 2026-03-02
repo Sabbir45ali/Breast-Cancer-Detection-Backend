61644bb5274362945c32b8ded235b0f379bab330
Sabbir45ali <sabbir84b@gmail.com>
2026-03-02 20:49:04 +0530

    chore: Add django-cors-headers to requirements and ensure newline at end of file

--- BCD_Models/backend/prediction/urls.py
+++ BCD_Models/backend/prediction/urls.py
@@ -3,11 +3,13 @@ from .views import (
     predict_image_api,
     org_predict_data,
     org_history,
-    image_history
+    image_history,
+    org_full_history
 )
 urlpatterns = [
     path('predict-image/', predict_image_api),
     path('org/predict-data/', org_predict_data),
     path('org/history/', org_history),
     path('image-history/', image_history),
+    path('org/full-history/', org_full_history),
 ]
